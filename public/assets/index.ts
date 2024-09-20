4e0c6d3f152d8cf992034371b5c353871e844fb9
felitech26 <felitech26@gmail.com>
2024-09-20 15:10:02 +0000

    Update index.ts

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -5,7 +5,7 @@ import ageImg from "./Images/ageImg.png";
 import online from "./Images/online.png";
 import bev from "./Images/bev.png";
 import united from "./Images/united.png";
-import bevcouture from "./Images/Bevcouture.png";
+import bevcouture from "./Images/bevcouture.png";
 
 
 export { logo, profileImg, amazonImg, ageImg, online, bev, united, bevcouture };
