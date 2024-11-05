81bdb9cb8e7856767893d381c7cfe89ae1d6fcf0
felitech26 <felitech26@gmail.com>
2024-11-05 14:13:39 +0000

    k

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,8 @@
 import {motion} from "framer-motion";
+import Image from "next/image";
+import { profileImg } from "@/public/assets";
+import { RxOpenInNewWindow } from 'react-icons/rx'
+import {bev, united } from '@/public/assets'
 
 // variants
 import {fadeIn} from '../variants'
@@ -6,20 +10,29 @@ import {fadeOut} from '../motion'
 
 const Banner = () => {
   return <section id="home"
-  className="max-w-contentContainer container mx-auto py-10 xl:py-15 flex flex-col
-  gap-4 lgl:gap-8 overflow-hidden">
+  className="w-full xl:h-screen px-3 flex justify-center items-center xl:px-[7rem] py-[8rem] lg:py-[7rem] xl:py-[12rem] overflow-hidden">
 
-  <div className="w-[450px] p-10 bg-[#151515] rounded-[15px]">
+<div className="w-full flex flex-col xl:flex-row justify-center items-center overflow-hidden gap-[3rem]">
+<div className="flex flex-col justify-center items-center">
+    <div className="mb-7">
+        <Image  
+        src={profileImg}
+        alt="profileImg"
+        className="w-[100px] "
+        />
+    </div> 
+    <div className="flex flex-col justify-center items-center">
   <motion.h1
-   className="text-3xl lgl:text-5xl font-titleFont font-bold flex flex-col">
-    Felix Abada <span className="text-textDark text-lg font-medium mt-2 lgl:mt-4 capitalize"> full stack web developer</span></motion.h1>
+   className="text-2xl lgl:text-5xl font-titleFont font-bold">
+    Felix Abada </motion.h1>
+  <p className="text-textDark text-lg font-medium mt-2 lgl:mt-4 capitalize"> full stack web developer</p>
 
   <motion.p 
   variants={fadeIn('up', 0.4)}
   initial="hidden"
   animate="show"
   exit="hidden"
-  className="text-base font-titleFont md:max-w-[500px] mt-2 font-medium"
+  className="text-base font-titleFont max-w-[400px] mt-2 font-medium"
   >
  PRODUCT DESIGN | BRANDING | STRATEGY   {" "}   
           
@@ -29,16 +42,17 @@ const Banner = () => {
   initial="hidden"
   animate="show"
   exit="hidden"
-  className="text-base md:max-w-[400px] text-textDark font-medium"
+  className="text-base w-[400px] text-textDark font-medium capitalize mt-3 text-center"
   >
   Driven by critical thinking and constant dedication, 
-  I ve had the opportunity to shape my path across diverse sectors, 
+  I have had the opportunity to shape my path across the web with my 
+  passion for Web Development
     {" "}   
           
  </motion.p>
  <a href="https://felitech-portfolio.vercel.app/" target="_blank">
      <span className="text-textGreen inline-flex relative cursor-pointer h-7
-     overflow-x-hidden group uppercase font-titleFont font-bold">
+     overflow-x-hidden group uppercase font-titleFont font-bold mt-5">
      My Resume
        <span className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1
        -translate-x-[110%] group-hover:translate-x-0 transition-transform
@@ -46,6 +60,64 @@ const Banner = () => {
    </span>
   </a>
   </div>
+
+
+    </div>
+
+
+<div className="w-full flex flex-col justify-center overflow-hidden">
+  <div><h1 className="mb-5 font-extrabold">PROJECTS</h1></div>
+  <div className="flex flex-col mdl:flex-row justify-center items-center gap-[2rem] xl:gap-[4rem]">
+  <div className="w-full md:w-[410px] py-4 bg-white/[0.015] border border-gray-700 
+  rounded-[10px] border-opacity-10">
+    <div className="flex justify-between items-center px-4">
+      <h1 className="text-[18px] font-titleFont font-bold text-white">Bev Couture</h1>
+      <a className='hover:text-textGreen duration-300' 
+            href="https://bev-couture.vercel.app/"
+             target='_blank'>
+              <RxOpenInNewWindow className="text-[20px]" />
+             </a>
+    </div>
+    <div className="px-4 pt-3"><Image className='w-full h-full object-cover'
+          src={bev}
+          alt='bev' /></div>
+    <div className="flex gap-4 px-4 mt-5 place-items-center">
+      <h1>Fashion</h1>
+      <h1>Ecommerce</h1>
+      <h1>Full Stack</h1>
+    </div>
+
+  </div>
+  <div className="w-full md:w-[410px] py-4 bg-white/[0.015] border border-gray-700 
+  rounded-[10px] border-opacity-10">
+    <div className="flex justify-between items-center px-4">
+      <h1 className="text-[18px] font-titleFont font-bold text-white">United Develpment Company</h1>
+      <a className='hover:text-textGreen duration-300' 
+            href="https://bev-couture.vercel.app/"
+             target='_blank'>
+              <RxOpenInNewWindow className="text-[20px]" />
+             </a>
+    </div>
+    <div className="px-4 pt-4"><Image className='w-full h-full object-cover'
+          src={united}
+          alt='united' /></div>
+    <div className="flex gap-4 px-4 mt-5 place-items-center">
+      <h1>Real Estate</h1>
+      <h1>Portfolio</h1>
+      <h1>Front End</h1>
+    </div>
+
+  </div>
+  </div>
+  </div>  
+
+</div>
+
+
+
+
+
+ 
  
  </section>
   
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,43 +1,20 @@
 import { logo, online } from "@/public/assets";
-import {useRef, useState} from "react"
 import Image from "next/image";
-import Link from "next/link";
+
 import { motion } from "framer-motion";
+import { AiOutlineYoutube } from 'react-icons/ai'
+import { TbBrandGithub } from 'react-icons/tb'
+
 
 
 // variants
 import {fadeIn} from '../variants'
-import LeftSide from "./LeftSide";
+
 
 const Navbar = () => {
-  const ref = useRef<string | any>("")
-  const [showMenu,setShowMenu]= useState(false);
-  const handleScroll =(e:React.MouseEvent<HTMLAnchorElement, MouseEvent>)=>{
-   e.preventDefault();
-   setShowMenu(false);
-   const href = e.currentTarget.href;
-   const targetId = href.replace(/.*\#/, "");
-   const elem = document.getElementById(targetId);
-   elem?.scrollIntoView({
-    behavior: "smooth",
-   });
-   // Update the class name of the clicked link
-   const links = document.querySelectorAll(".nav-link")
-   links.forEach((link) => {
-    link.classList.remove("active")
-   });
-   e.currentTarget.classList.add("active");
- };
-
- function handleClick(e:any){
-  if(e.target.contains(ref.current)){
-    // do something with myRef.current
-    setShowMenu(false);
-  }
- }
-
+ 
   return  (
-  <div className="w-full h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor 
+  <div className="w-full h-20 lg:h-[12vh] fixed top-0 z-50 bg-bodyColor 
   px-4">
    <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center
       justify-between">
@@ -49,53 +26,31 @@ const Navbar = () => {
         <Image src={logo} alt="logo"
         className="w-[35px]"/>
         </motion.div>
-        <div className="hidden sml:inline-flex justify-center items-center gap-3">
+
+        <div className='text-2xl flex gap-4'>
+             <a className='hover:text-textGreen duration-300' 
+             href="https://github.com/Felitech26/felitech-rolls-royce"
+             target='_blank'>
+              <TbBrandGithub />
+             </a>
+             <a className='hover:text-textGreen duration-300' 
+             href="https://www.youtube.com/"
+             target='_blank'>
+              <AiOutlineYoutube />
+             </a>
+             <a className='hover:text-textGreen duration-300' 
+             href="https://github.com/Felitech26/felitech-rolls-royce"
+             target='_blank'>
+              <TbBrandGithub />
+             </a>
+          </div>
+
+        <div className="flex justify-center items-center gap-3">
           <Image src={online} alt="online" 
           className="w-[8px] mb-1"/> 
           <h4 className="font-titleFont font-semibold">Available for work</h4>
         </div>
         
-       <span className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1
-       -translate-x-[110%] group-hover:translate-x-0 transition-transform
-       duration-500"></span>
- <div className="flex items-center gap-7 mt-3">
-          <ul className="flex text-[12px]  gap-7">
-
-  <Link href="#project"
-          onClick={handleScroll}>
-     <span 
-     className="font-bold uppercase text-textDark inline-flex relative cursor-pointer h-7
-     overflow-x-hidden group nav-link">
-      PROJECTS
-       <span className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1
-       -translate-x-[110%] group-hover:translate-x-0 transition-transform
-       duration-500"></span>
-   </span>
-  </Link>  
-  <Link href="#about"
-          onClick={handleScroll}>
-     <span
-     className="font-bold uppercase text-textDark inline-flex relative cursor-pointer h-7
-     overflow-x-hidden group nav-link">
-     ABOUT
-       <span className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1
-       -translate-x-[110%] group-hover:translate-x-0 transition-transform
-       duration-500"></span>
-   </span>
-  </Link>  
-  <Link href="#contact"
-          onClick={handleScroll}>
-     <span 
-     className="font-bold uppercase text-textDark inline-flex relative cursor-pointer h-7
-     overflow-x-hidden group ">
-      CONTACT
-       <span className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1
-       -translate-x-[110%] group-hover:translate-x-0 transition-transform
-       duration-500"></span>
-   </span>
-  </Link>       
-         </ul>
-        </div>
 
       </div>
     </div>
