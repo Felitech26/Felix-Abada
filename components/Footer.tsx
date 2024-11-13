29a2d9603def64045131c9954317bb5ba99866d6
felitech26 <felitech26@gmail.com>
2024-11-13 14:57:40 +0000

    l

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full bottom-0 py-2 relative overflow-hidden bg-bodyColor xl:hidden'>
+    </footer>
+  )
+}
+
+export default Footer
