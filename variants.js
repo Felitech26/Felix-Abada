4c9c71a7cfe8ecad6c47ac90ee7bc4a07d7d2197
felitech26 <felitech26@gmail.com>
2025-04-21 00:46:33 +0000

    l

--- variants.js
+++ variants.js
@@ -24,3 +24,71 @@ export const fadeIn = (direction, delay) => {
     },
   };
 };
+
+export const staggerContainer = (staggerChildren, delayChildren) => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren,
+        delayChildren,
+      },
+    },
+  };
+};
+
+export const textVariant = (delay) => {
+  return {
+    hidden: {
+      y: 50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        duration: 1.25,
+        delay,
+      },
+    },
+  };
+};
+
+export const slideIn = (direction, type, delay, duration) => {
+  return {
+    hidden: {
+      x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
+      y: direction === 'up' ? '100%' : direction === 'down' ? '100%' : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type,
+        delay,
+        duration,
+        ease: 'easeOut',
+      },
+    },
+  };
+};
+
+export const scaleIn = (delay) => {
+  return {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: 'tween',
+        delay,
+        duration: 0.8,
+        ease: [0.34, 1.56, 0.64, 1],
+      },
+    },
+  };
+};
